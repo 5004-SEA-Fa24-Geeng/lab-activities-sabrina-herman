4d8de0e611c3f3add30be3436626aba72dfea9c2
Sabrina Herman <sabrinajherman@gmail.com>
2025-02-13 11:32:16 -0800

    Lab 5

--- Module05/MapPractice.java
+++ Module05/MapPractice.java
@@ -17,14 +17,35 @@ public class MapPractice {
 
         // what happens if we add a duplicate key?
         // your code here
+        Map<Person, Double> personMap = new HashMap<>();
+        Person p1 = new Person("Alice");
+        Person p2 = new Person("Bob");
+        Person p3 = new Person("Charlie");
+        Person p4 = new Person("Bob");
 
+        personMap.put(p1, 1.0);
+        personMap.put(p2, 2.0);
+        personMap.put(p3, 3.0);
+        personMap.put(p4, 4.0);
+
+        System.out.println(personMap);
 
         // now try it with Person/Student. We will
         // use the person or student as a key and the
         // value will be a set of classes they are taking
 
         Map<Person, Set<String>> classes = new HashMap<>();
+        Person p5 = new Person("Alice");
+        Person p6 = new Person("Bob");
+        Person p7 = new Person("Charlie");
+        Person p8 = new Person("Bob");
 
+        classes.put(p5, Set.of("CS5001", "CS5002"));
+        classes.put(p6, Set.of("CS5001", "CS5002"));
+        classes.put(p7, Set.of("CS5001", "CS5002"));
+        classes.put(p8, Set.of("CS5001", "CS5002"));
+
+        System.out.println(classes);
         // you will find Set.of() useful here
         // for example:
         // classes.put(p2, Set.of("CS5001", "CS5002"));
